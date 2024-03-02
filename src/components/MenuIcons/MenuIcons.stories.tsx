d94933f2cad12da29b049e5eba3737d7f6166769
Evandro Calado <evandrocalado07@gmail.com>
2024-03-02 16:52:40 -0300

    🐳 chore(components): add stories

---
+++ src/components/MenuIcons/MenuIcons.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/react';
+import { MenuIcons } from '.';
+
+export default {
+  title: 'Components/MenuIcons',
+  component: MenuIcons,
+} as Meta;
+
+export const Default = () => <MenuIcons />;
