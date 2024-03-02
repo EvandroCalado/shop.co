1ea56bc5fa6b3574fbb07fdb687a90614ca3449a
Evandro Calado <evandrocalado07@gmail.com>
2024-03-02 15:47:51 -0300

    🐳 chore(components): add stories

---
+++ src/components/NavLinks/NavLinks.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/react';
+import { NavLinks } from '.';
+
+export default {
+  title: 'Components/NavLinks',
+  component: NavLinks,
+} as Meta;
+
+export const Default = () => <NavLinks />;
