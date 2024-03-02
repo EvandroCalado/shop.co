fad6f2293734cc8c8c1b4160da16dccf927235a6
Evandro Calado <evandrocalado07@gmail.com>
2024-03-02 15:02:46 -0300

    ✨ feat(components): create logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export const Logo = () => {
+  return (
+    <Link href="/" className="text-2xl font-black uppercase">
+      <h1>shop.co</h1>
+    </Link>
+  );
+};
