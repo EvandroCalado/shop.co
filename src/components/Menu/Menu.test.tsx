e70b733404a1599bbb9838218e70704ff80ca0a0
Evandro Calado <evandrocalado07@gmail.com>
2024-03-02 21:05:28 -0300

    🧪 test(components): add test

---
+++ src/components/Menu/Menu.test.tsx
@@ -0,0 +1,55 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { useState } from 'react';
+import { Menu } from '.';
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}));
+
+describe('<Menu />', () => {
+  it('should render with default values', () => {
+    const mockSetShow = jest.fn();
+    useState.mockReturnValue([false, mockSetShow]);
+
+    render(<Menu />);
+
+    expect(screen.getByTestId('menu')).toBeInTheDocument();
+    expect(screen.getByTestId('menu-container')).toBeInTheDocument();
+    expect(screen.getByTestId('menu-container')).toHaveClass('invisible');
+    expect(screen.getByTestId('close')).toBeInTheDocument();
+  });
+
+  it('should render menu container visible', () => {
+    const mockSetShow = jest.fn();
+    useState.mockReturnValue([true, mockSetShow]);
+
+    render(<Menu />);
+
+    const menuButton = screen.getByTestId('menu');
+
+    fireEvent.click(menuButton);
+
+    expect(screen.getByTestId('menu-container')).toHaveClass('visible');
+  });
+
+  it('should render menu container invisible', () => {
+    const mockSetShow = jest.fn();
+    useState.mockReturnValue([false, mockSetShow]);
+
+    render(<Menu />);
+
+    expect(screen.getByTestId('menu-container')).toHaveClass('invisible');
+
+    const closeButton = screen.getByTestId('close');
+
+    fireEvent.click(closeButton);
+  });
+
+  it('should render a snapshot', () => {
+    const { container } = render(<Menu />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
