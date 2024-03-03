62f74eeec17e91f68d36ce244a8d80f047114e75
Evandro Calado <evandrocalado07@gmail.com>
2024-03-03 17:14:36 -0300

    🐳 chore(add stories):

---
+++ src/components/BrandsBar/BrandsBar.stories.tsx
@@ -0,0 +1,11 @@
+import { BrandsBar } from '.';
+
+export default {
+  title: 'components/BrandsBar',
+  component: BrandsBar,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = () => <BrandsBar />;
