fb81bda6be93002cbc691b55322ce04d5d9c6df3
Evandro Calado <evandrocalado07@gmail.com>
2024-03-02 21:05:04 -0300

    ✨ feat(components): create menu

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Menu as MenuIcon, X } from 'lucide-react';
+import Link from 'next/link';
+import { useState } from 'react';
+import { menuLinks } from '../../utils/menuLinks';
+
+export const Menu = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <div className="md:hidden">
+      <button onClick={() => setShowMenu(!showMenu)} data-testid="menu">
+        <MenuIcon />
+      </button>
+
+      <div
+        className={`${showMenu ? 'visible opacity-100' : 'invisible opacity-0'} absolute inset-0 z-10 m-auto flex h-[95%] w-[95%] flex-col items-center justify-center gap-4 bg-[#f0f0f0] capitalize transition-all duration-300`}
+        data-testid="menu-container"
+      >
+        <button
+          onClick={() => setShowMenu(!showMenu)}
+          className="absolute left-3 top-4 sm:left-[7px]"
+          data-testid="close"
+        >
+          <X className="text-red-600" />
+        </button>
+
+        {menuLinks.map((link) => (
+          <Link
+            key={link.id}
+            href={link.href}
+            className="cursor-pointer duration-150 hover:text-zinc-600"
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
