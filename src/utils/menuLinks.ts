76f218f4a02a8e6601f14996aa578dacfafdede5
Evandro Calado <evandrocalado07@gmail.com>
2024-03-02 21:07:01 -0300

    🐳 chore(utils): menu links

---
+++ src/utils/menuLinks.ts
@@ -0,0 +1,22 @@
+export const menuLinks = [
+  {
+    id: 1,
+    name: 'shop',
+    href: '/shop',
+  },
+  {
+    id: 2,
+    name: 'on sale',
+    href: '/on-sale',
+  },
+  {
+    id: 3,
+    name: 'new arrivals',
+    href: '/new-arrivals',
+  },
+  {
+    id: 4,
+    name: 'brands',
+    href: '/brands',
+  },
+];
