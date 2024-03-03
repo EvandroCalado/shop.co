34cf0ef7d5b7a76a84da7e3b52840b1e652f4e18
Evandro Calado <evandrocalado07@gmail.com>
2024-03-02 21:06:26 -0300

    🐞 fix(components): move links for utils

--- src/components/NavLinks/NavLinks.tsx
+++ src/components/NavLinks/NavLinks.tsx
@@ -1,32 +1,10 @@
 import Link from 'next/link';
-
-const links = [
-  {
-    id: 1,
-    name: 'shop',
-    href: '/shop',
-  },
-  {
-    id: 2,
-    name: 'on sale',
-    href: '/on-sale',
-  },
-  {
-    id: 3,
-    name: 'new arrivals',
-    href: '/new-arrivals',
-  },
-  {
-    id: 4,
-    name: 'brands',
-    href: '/brands',
-  },
-];
+import { menuLinks } from '../../utils/menuLinks';
 
 export const NavLinks = () => {
   return (
     <nav className="hidden items-center gap-2 capitalize md:flex">
-      {links.map((link) => (
+      {menuLinks.map((link) => (
         <Link
           key={link.id}
           href={link.href}
