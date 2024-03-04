56d3c639fed9a3153b6f9a4f72e17fb14b0723d1
Evandro Calado <evandrocalado07@gmail.com>
2024-03-03 22:31:16 -0300

    🐳 chore(components): add stories

---
+++ src/components/BrowseBy/BrowseBy.stories.tsx
@@ -0,0 +1,11 @@
+import { BrowseBy } from '.';
+
+export default {
+  title: 'Components/BrowseBy',
+  component: BrowseBy,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = () => <BrowseBy />;
