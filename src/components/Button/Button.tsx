42757396ef3a2043b6064dba9e90fdff60e9934e
Evandro Calado <evandrocalado07@gmail.com>
2024-03-04 21:16:31 -0300

    🌈 style(components): change hover

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -16,7 +16,7 @@ export const Button: FC<ButtonProps> = ({
   return (
     <button
       className={twMerge(
-        `${variant === 'primary' ? 'bg-black/95 text-white hover:bg-black/85' : 'border border-black/95 bg-white text-black/95 hover:brightness-95'} rounded-full px-14 py-3 text-sm capitalize transition-all duration-150`,
+        `${variant === 'primary' ? 'bg-black/95 text-white hover:bg-black/85' : 'border border-black/95 bg-white text-black/95 hover:bg-[#F0f0f0]'} rounded-full px-14 py-3 text-sm capitalize transition-all duration-150`,
         className,
       )}
       {...rest}
