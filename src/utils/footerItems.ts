3705195eedefb51a0e1a02a354853db0ddacb0db
Evandro Calado <evandrocalado07@gmail.com>
2024-03-04 23:31:02 -0300

    🐳 chore(utils): create footer items

---
+++ src/utils/footerItems.ts
@@ -0,0 +1,91 @@
+export const company = [
+  {
+    id: 1,
+    name: 'about',
+    href: '/about',
+  },
+  {
+    id: 2,
+    name: 'features',
+    href: '/features',
+  },
+  {
+    id: 3,
+    name: 'works',
+    href: '/works',
+  },
+  {
+    id: 4,
+    name: 'career',
+    href: '/career',
+  },
+];
+
+export const help = [
+  {
+    id: 1,
+    name: 'customer support',
+    href: '/customer-support',
+  },
+  {
+    id: 2,
+    name: 'delivery details',
+    href: '/delivery-details',
+  },
+  {
+    id: 3,
+    name: 'terms & conditions',
+    href: '/terms-conditions',
+  },
+  {
+    id: 4,
+    name: 'privacy policy',
+    href: '/privacy-policy',
+  },
+];
+
+export const faq = [
+  {
+    id: 1,
+    name: 'account',
+    href: '/account',
+  },
+  {
+    id: 2,
+    name: 'manage deliveries',
+    href: '/manage-deliveries',
+  },
+  {
+    id: 3,
+    name: 'orders',
+    href: '/orders',
+  },
+  {
+    id: 4,
+    name: 'payments',
+    href: '/payments',
+  },
+];
+
+export const resources = [
+  {
+    id: 1,
+    name: 'free ebooks',
+    href: '/free-ebooks',
+  },
+  {
+    id: 2,
+    name: 'development tutorial',
+    href: '/development-tutorial',
+  },
+  {
+    id: 3,
+    name: 'how to - blog',
+    href: '/how-to-blog',
+  },
+  {
+    id: 4,
+    name: 'youtube playlist',
+    href: '/youtube-playlist',
+  },
+];
