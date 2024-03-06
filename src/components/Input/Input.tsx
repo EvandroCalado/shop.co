0aff5f8d13c9261ed763437c69530a5531ec8406
Evandro Calado <evandrocalado07@gmail.com>
2024-03-05 21:54:12 -0300

    🦄 refactor(components): add register, styles

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,24 +1,28 @@
 import React, { FC, InputHTMLAttributes } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   variant?: 'default' | 'contrast';
   icon?: React.ReactNode;
+  register?: UseFormRegisterReturn;
 }
 
 export const Input: FC<InputProps> = ({
   variant = 'default',
   icon,
+  register,
   ...rest
 }) => {
   return (
     <div
-      className={`${variant === 'default' ? 'bg-white' : 'bg-[#f0f0f0]'} flex w-full items-center gap-2 rounded-full px-4 py-[11px] [&>svg]:text-zinc-500`}
+      className={`relative [&>svg]:absolute [&>svg]:left-2 [&>svg]:top-2 [&>svg]:z-10 [&>svg]:text-zinc-400`}
     >
-      {icon && icon}
       <input
-        className="w-full bg-transparent px-2 text-sm outline-none"
+        className={`${variant === 'default' ? 'bg-white' : 'bg-[#f0f0f0]'} w-full rounded-full px-10 py-[11px] text-sm outline-none ring-black/95 duration-150 focus:ring-1 [&+svg]:duration-150 [&+svg]:focus:text-black/95`}
         {...rest}
+        {...register}
       />
+      {icon && icon}
     </div>
   );
 };
