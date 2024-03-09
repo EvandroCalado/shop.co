7da29e4135637460e2c40e232df12221e615b3b9
Evandro Calado <evandrocalado07@gmail.com>
2024-03-09 20:02:02 -0300

    🐳 chore(components): add tw merge

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,17 @@
 import Link from 'next/link';
+import { FC } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export const Logo = () => {
+interface LogoProps {
+  className?: string;
+}
+
+export const Logo: FC<LogoProps> = ({ className }) => {
   return (
-    <Link href="/" className="text-2xl font-black uppercase">
+    <Link
+      href="/"
+      className={twMerge('text-2xl font-black uppercase sm:block', className)}
+    >
       <h1>shop.co</h1>
     </Link>
   );
