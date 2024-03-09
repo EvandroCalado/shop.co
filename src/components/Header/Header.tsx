f6dd951c050cd0db2e1c35578094d2c32bcfa45b
Evandro Calado <evandrocalado07@gmail.com>
2024-03-09 20:18:43 -0300

    🌈 style(components): fix responsively

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,14 @@ import { Logo, Menu, MenuIcons, NavLinks, Search } from '..';
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between gap-8 px-6 py-6 md:px-16 lg:px-24">
+    <header className="flex items-center justify-between gap-2 px-6 py-6 sm:gap-8 md:px-16 lg:px-24">
       <div className="flex items-center gap-4">
         <Menu />
-        <Logo />
+        <Logo className="hidden sm:block" />
         <NavLinks />
       </div>
 
-      <div className="flex items-center gap-4 lg:w-1/2">
+      <div className="flex items-center gap-2 sm:gap-4 lg:w-1/2">
         <Search />
         <MenuIcons />
       </div>
