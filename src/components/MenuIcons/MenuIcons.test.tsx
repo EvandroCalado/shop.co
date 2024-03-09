62e0621dd07cb4f34430643e35f090207f9b372c
Evandro Calado <evandrocalado07@gmail.com>
2024-03-09 19:59:23 -0300

    🧪 test(components): add session provider

--- src/components/MenuIcons/MenuIcons.test.tsx
+++ src/components/MenuIcons/MenuIcons.test.tsx
@@ -1,18 +1,47 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { SessionProvider } from 'next-auth/react';
+import { vi } from 'vitest';
 import { MenuIcons } from '.';
 
 describe('<MenuIcons />', () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
   it('should render with default values', () => {
-    render(<MenuIcons />);
+    render(
+      <SessionProvider>
+        <MenuIcons />
+      </SessionProvider>,
+    );
 
     expect(screen.getByTestId('shop')).toBeInTheDocument();
     expect(screen.getByText(/2/i)).toBeInTheDocument();
-    expect(screen.getByTestId('user')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'login' })).toBeInTheDocument();
   });
 
+  // it('should render with user logged in', () => {
+  //   const mockSession = { user: 'John Doe' };
+  //   vi.mock('next-auth', () => ({
+  //     useSession: vi.fn(() => ({ data: mockSession })),
+  //   }));
+
+  //   render(
+  //     <SessionProvider>
+  //       <MenuIcons />
+  //     </SessionProvider>,
+  //   );
+
+  //   expect(screen.getByTestId('user')).toBeInTheDocument();
+  // });
+
   it('should render a snapshot', () => {
-    const { container } = render(<MenuIcons />);
+    const { container } = render(
+      <SessionProvider>
+        <MenuIcons />
+      </SessionProvider>,
+    );
 
     expect(container).toMatchSnapshot();
   });
