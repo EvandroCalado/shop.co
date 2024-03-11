336e35ca6ab79e06e8e44e0aae8529fa179b9879
Evandro Calado <evandrocalado07@gmail.com>
2024-03-10 21:14:00 -0300

    🐳 chore(pages): fetch all products

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { getAll } from '@/actions/products';
 import {
   BrandsBar,
   BrowseBy,
@@ -6,14 +7,17 @@ import {
   ProductsCarousel,
   RatingCarousel,
 } from '@/components';
+import { ProductsType } from '@/types/productsType';
 
-export default function Home() {
+export default async function Home() {
+  const products: ProductsType = await getAll();
+
   return (
     <>
       <Hero />
       <BrandsBar />
-      <ProductsCarousel title="new arrivals" />
-      <ProductsCarousel title="top selling" />
+      <ProductsCarousel title="new arrivals" products={products} />
+      <ProductsCarousel title="top selling" products={products} />
       <BrowseBy />
       <RatingCarousel />
       <NewsLetter />
