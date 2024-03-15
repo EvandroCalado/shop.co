16c066b51e1c8d641c3f8dc518f820a74b0dbac0
Evandro Calado <evandrocalado07@gmail.com>
2024-03-15 20:44:49 -0300

    ✨ feat(components): create bread crumb

---
+++ src/components/BreadCrumb/BreadCrumb.tsx
@@ -0,0 +1,32 @@
+import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
+import { FC } from 'react';
+
+export interface BreadCrumbProps {
+  name: string;
+  id: number;
+}
+
+export const BreadCrumb: FC<BreadCrumbProps> = ({ name, id }) => {
+  return (
+    <div className="my-8 flex items-center gap-1 text-sm text-zinc-400">
+      <span className="flex items-center gap-1">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <ChevronRight size={16} />
+      </span>
+      <span className="flex items-center gap-1">
+        <Link href="/shop" className="hover:underline">
+          Shop
+        </Link>
+        <ChevronRight size={16} />
+      </span>
+      <span className="flex items-center capitalize">
+        <Link href={`/product/${id}`} className="hover:underline">
+          {name}
+        </Link>
+      </span>
+    </div>
+  );
+};
