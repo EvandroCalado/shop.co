701dcfa64a7ce699e4181f0c2e1aa175d990ff2e
Evandro Calado <evandrocalado07@gmail.com>
2024-03-15 10:47:10 -0300

    🐳 chore(components): move price
    move price to a component

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,10 +1,9 @@
 import { ProductType } from '@/types/productsType';
-import { calcDiscount } from '@/utils/calcDiscount';
 import { Star, StarHalf } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
-import { Heading } from '..';
+import { Heading, ProductPrice } from '..';
 
 export interface ProductCardProps {
   product: ProductType;
@@ -38,17 +37,7 @@ export const ProductCard: FC<ProductCardProps> = ({ product }) => {
         <span>4.5/5</span>
       </div>
 
-      <div className="flex items-center gap-2">
-        <span className="text-4xl font-semibold">
-          ${calcDiscount(price, discount)}
-        </span>
-        <span className="text-4xl font-semibold text-zinc-400 line-through">
-          ${price}
-        </span>
-        <span className="rounded-full bg-[#f0f0f0] px-3 py-1 text-[12px] text-red-500">
-          -{discount}%
-        </span>
-      </div>
+      <ProductPrice price={price} discount={discount} />
     </Link>
   );
 };
