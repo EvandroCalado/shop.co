52e707b26678a20a4a8736a28e9e71362d52d0ae
Evandro Calado <evandrocalado07@gmail.com>
2024-03-15 23:30:08 -0300

    🦄 refactor(components): if not discount

--- src/components/ProductPrice/ProductPrice.tsx
+++ src/components/ProductPrice/ProductPrice.tsx
@@ -3,21 +3,23 @@ import { FC } from 'react';
 
 export interface ProductPriceProps {
   price: number;
-  discount: number;
+  discount?: number;
 }
 
 export const ProductPrice: FC<ProductPriceProps> = ({ price, discount }) => {
   return (
     <div className="flex items-center gap-2">
       <span className="text-4xl font-semibold">
-        ${calcDiscount(price, discount)}
+        ${discount ? calcDiscount(price, discount) : price}
       </span>
       <span className="text-4xl font-semibold text-zinc-400 line-through">
         ${price}
       </span>
-      <span className="rounded-full bg-[#f0f0f0] px-3 py-1 text-[12px] text-red-500">
-        -{discount}%
-      </span>
+      {discount && (
+        <span className="rounded-full bg-[#f0f0f0] px-3 py-1 text-[12px] text-red-500">
+          -{discount}%
+        </span>
+      )}
     </div>
   );
 };
