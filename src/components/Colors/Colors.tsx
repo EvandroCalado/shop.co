6a66d4e818f173f754f9870a5b9bb9b8afae91db
Evandro Calado <evandrocalado07@gmail.com>
2024-03-15 22:57:40 -0300

    🐳 chore(components): add active state

--- src/components/Colors/Colors.tsx
+++ src/components/Colors/Colors.tsx
@@ -1,21 +1,31 @@
+'use client';
+
 import { ProductType } from '@/types/productsType';
-import { FC } from 'react';
+import { Check } from 'lucide-react';
+import { FC, useState } from 'react';
 
 export interface ColorsProps {
   colors: ProductType['attributes']['colors'];
 }
 
 export const Colors: FC<ColorsProps> = ({ colors }) => {
+  const [active, setActive] = useState(colors.data[0].attributes.name);
+
   return (
     <div>
       <span className="text-zinc-400">Select Colors:</span>
       <div className="mt-2 flex items-center gap-2">
         {colors.data.map((color) => (
-          <div
+          <button
             key={color.id}
             style={{ backgroundColor: color.attributes.color }}
-            className="h-8 w-8 rounded-full"
-          ></div>
+            className="flex h-8 w-8 items-center justify-center rounded-full"
+            onClick={() => setActive(color.attributes.name)}
+          >
+            {active === color.attributes.name && (
+              <Check size={16} color="white" />
+            )}
+          </button>
         ))}
       </div>
     </div>
--- src/components/Sizes/Sizes.tsx
+++ src/components/Sizes/Sizes.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { ProductType } from '@/types/productsType';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Button } from '..';
 
 export interface SizeProps {
@@ -7,6 +9,8 @@ export interface SizeProps {
 }
 
 export const Sizes: FC<SizeProps> = ({ sizes }) => {
+  const [active, setActive] = useState(sizes.data[0].attributes.name);
+
   return (
     <div>
       <span className="text-zinc-400">Choose Size:</span>
@@ -15,7 +19,8 @@ export const Sizes: FC<SizeProps> = ({ sizes }) => {
           <Button
             key={size.id}
             variant="outline"
-            className="border-none bg-[#f0f0f0] px-4 py-2 outline-none hover:bg-black/95 hover:text-white"
+            className={`${active === size.attributes.name ? 'bg-black/95 text-white' : 'bg-[#f0f0f0]'} border-none px-4 py-2 outline-none hover:bg-black/95 hover:text-white`}
+            onClick={() => setActive(size.attributes.name)}
           >
             {size.attributes.name}
           </Button>
