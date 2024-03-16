45920b839d2e09c2da09da08178881be8b4d4773
Evandro Calado <evandrocalado07@gmail.com>
2024-03-15 23:11:48 -0300

    📖 story(components): add stories

---
+++ src/components/Colors/Colors.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { Colors, ColorsProps } from '.';
+
+export default {
+  title: 'Components/ Colors',
+  component: Colors,
+  args: {
+    colors: {
+      data: [
+        {
+          id: 1,
+          attributes: {
+            name: 'black',
+            color: '#000000',
+          },
+        },
+        {
+          id: 2,
+          attributes: {
+            name: 'white',
+            color: '#ffffff',
+          },
+        },
+        {
+          id: 2,
+          attributes: {
+            name: 'red',
+            color: '#ff0000',
+          },
+        },
+      ],
+    },
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as Meta;
+
+export const Default: StoryFn<ColorsProps> = (args) => <Colors {...args} />;
