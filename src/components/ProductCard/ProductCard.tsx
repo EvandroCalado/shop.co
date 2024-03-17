09bb435bd094dc8e47c177e31488ed24c39735a7
Evandro Calado <evandrocalado07@gmail.com>
2024-03-17 19:45:56 -0300

    🌈 style(components): change hover on card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -15,14 +15,14 @@ export const ProductCard: FC<ProductCardProps> = ({ product }) => {
   return (
     <Link
       href={`/product/${product.id}`}
-      className="flex snap-start flex-col gap-3 [&>div>img]:duration-300 hover:[&>div>img]:scale-110"
+      className="group flex snap-start flex-col gap-3 [&>div>img]:duration-300"
     >
       <div className="relative h-[350px] w-[360px] overflow-hidden rounded-xl bg-[#f0f0f0]">
         <Image
           src={cover.data.attributes.formats.small.url}
           alt={name}
           fill
-          className="object-contain"
+          className="object-contain group-hover:scale-110"
         />
       </div>
 
