2e441d3145ad2cb9df358a980eb2be745bce50a3
Evandro Calado <evandrocalado07@gmail.com>
2024-03-17 22:50:47 -0300

    🐳 chore(components): add count

--- src/components/Quantity/Quantity.tsx
+++ src/components/Quantity/Quantity.tsx
@@ -1,10 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+
 export const Quantity = () => {
+  const [count, setCount] = useState(1);
+
   return (
     <div>
-      <div className="flex items-center gap-4 overflow-hidden rounded-full bg-[#f0f0f0] text-xl">
-        <button className="flex-1 px-4 py-2 hover:bg-zinc-400/10">-</button>
-        <span>1</span>
-        <button className="flex-1 px-4 py-2 hover:bg-zinc-400/10">+</button>
+      <div className="flex items-center gap-2 overflow-hidden rounded-full bg-[#f0f0f0] text-xl">
+        <button
+          className="flex-1 px-4 py-2 hover:bg-zinc-400/10"
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+        <span className="w-6 text-center">{count}</span>
+        <button
+          className="flex-1 px-4 py-2 hover:bg-zinc-400/10"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
       </div>
     </div>
   );
