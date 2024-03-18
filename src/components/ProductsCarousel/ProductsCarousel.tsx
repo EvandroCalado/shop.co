7cf0e613e6de4512d39ce8da4c27c7225ec37f4a
Evandro Calado <evandrocalado07@gmail.com>
2024-03-17 23:22:35 -0300

    🐳 chore(components): add tw merge

--- src/components/ProductsCarousel/ProductsCarousel.tsx
+++ src/components/ProductsCarousel/ProductsCarousel.tsx
@@ -1,18 +1,21 @@
 import { ProductsType } from '@/types/productsType';
 import { FC } from 'react';
+import { twMerge } from 'tailwind-merge';
 import { Button, Heading, ProductCard } from '..';
 
 export interface ProductsCarouselProps {
   products: ProductsType;
   title: string;
+  className?: string;
 }
 
 export const ProductsCarousel: FC<ProductsCarouselProps> = ({
   products,
   title,
+  className,
 }) => {
   return (
-    <section className="my-8 space-y-8 px-6 py-6 md:px-16 lg:px-24">
+    <section className={twMerge('my-8 space-y-8', className)}>
       <Heading title={title} uppercase className="text-center" />
 
       <div className="flex w-full snap-mandatory items-center justify-between gap-8 overflow-y-hidden overflow-x-scroll scrollbar-hide">
