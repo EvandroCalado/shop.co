e04c96ffebbc7e925618cdddfbb8b32dae3a120b
Evandro Calado <evandrocalado07@gmail.com>
2024-03-18 22:20:36 -0300

    🐳 chore(utils): create rating stars

---
+++ src/utils/ratingStars.ts
@@ -0,0 +1,27 @@
+export const ratingStars = [
+  {
+    id: 1,
+    rate: 1,
+    rateName: 'one',
+  },
+  {
+    id: 2,
+    rate: 2,
+    rateName: 'two',
+  },
+  {
+    id: 3,
+    rate: 3,
+    rateName: 'three',
+  },
+  {
+    id: 4,
+    rate: 4,
+    rateName: 'four',
+  },
+  {
+    id: 5,
+    rate: 5,
+    rateName: 'five',
+  },
+];
