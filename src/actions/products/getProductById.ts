2d52bc696f2dd818d3180c5b4d22b001e1917507
Evandro Calado <evandrocalado07@gmail.com>
2024-03-19 15:24:41 -0300

    🐳 chore(actions): add populate deep

--- src/actions/products/getProductById.ts
+++ src/actions/products/getProductById.ts
@@ -1,7 +1,7 @@
 import { customFetch } from '@/utils/customFetch';
 
 export const getProductById = async (id: string) => {
-  const populate = '?populate=*';
+  const populate = '?populate=deep,3';
 
   const { data } = await customFetch.get(`/products/${id}${populate}`);
 
