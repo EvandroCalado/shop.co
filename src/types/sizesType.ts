33e286a076ae040949f4604ac97966948e831602
Evandro Calado <evandrocalado07@gmail.com>
2024-03-20 21:13:57 -0300

    🐳 chore(types): create sizes type

---
+++ src/types/sizesType.ts
@@ -0,0 +1,28 @@
+export interface SizesType {
+  data: Datum[];
+  meta: Meta;
+}
+
+interface Meta {
+  pagination: Pagination;
+}
+
+interface Pagination {
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
+}
+
+interface Datum {
+  id: number;
+  attributes: Attributes;
+}
+
+interface Attributes {
+  name: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
