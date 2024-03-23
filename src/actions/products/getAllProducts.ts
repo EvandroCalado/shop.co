4cba9c387a691e646ba90c53a081450f870d8b03
Evandro Calado <evandrocalado07@gmail.com>
2024-03-22 23:24:30 -0300

    🐳 chore(actions): add filters

--- src/actions/products/getAllProducts.ts
+++ src/actions/products/getAllProducts.ts
@@ -1,9 +1,33 @@
 import { customFetch } from '@/utils/customFetch';
 import { AxiosError } from 'axios';
 
-export const getAllProducts = async () => {
+export const getAllProducts = async (
+  activeClothe = '',
+  activePrice = '',
+  activeColor = '',
+  activeSize = '',
+  activeDressStyle = '',
+  page = '1',
+) => {
+  const populate = '?populate=deep,3';
+  const clotheFilter =
+    activeClothe && `&filters[categories][slug][$eq]=${activeClothe}`;
+  const priceFilter = activePrice && `&filters[price][$lte]=${activePrice}`;
+  const colorFilter =
+    activeColor && `&filters[colors][slug][$eq]=${activeColor}`;
+  const sizeFilter = activeSize && `&filters[sizes][slug][$eq]=${activeSize}`;
+  const dressStyleFilter =
+    activeDressStyle && `&filters[dress_style][slug][$eq]=${activeDressStyle}`;
+  const pagination = page
+    ? `&pagination[page]=${page}&pagination[pageSize]=6`
+    : '&pagination[page]=1&pagination[pageSize]=6';
+
+  const url = `${populate}${clotheFilter}${priceFilter}${colorFilter}${sizeFilter}${dressStyleFilter}${pagination}`;
+
+  // console.log(url);
+
   try {
-    const { data } = await customFetch.get('/products?populate=*');
+    const { data } = await customFetch.get(`/products${url}`);
 
     if (!data) {
       throw new Error('No products found');
