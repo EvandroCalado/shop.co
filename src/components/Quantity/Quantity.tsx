b5469d0b0e6dcbc6b4950ac76b713c7bef8e2dd6
Evandro Calado <evandrocalado07@gmail.com>
2024-03-25 20:30:33 -0300

    🦄 refactor(components): add responsivity

--- src/components/Quantity/Quantity.tsx
+++ src/components/Quantity/Quantity.tsx
@@ -5,6 +5,7 @@ import { FC } from 'react';
 export interface QuantityProps {
   activeQuantity: number;
   setActiveQuantity: (quantity: number) => void;
+  isSmall?: boolean;
 }
 
 export const Quantity: FC<QuantityProps> = ({
@@ -17,16 +18,16 @@ export const Quantity: FC<QuantityProps> = ({
 
   return (
     <div>
-      <div className="flex items-center gap-2 overflow-hidden rounded-full bg-[#f0f0f0] text-xl">
+      <div className="flex items-center gap-2 overflow-hidden rounded-full bg-[#f0f0f0] text-base md:text-xl">
         <button
-          className="flex-1 px-4 py-2 hover:bg-zinc-400/10"
+          className="flex-1 px-2 py-1 hover:bg-zinc-400/10 md:px-4 md:py-2"
           onClick={() => setActiveQuantity(activeQuantity - 1)}
         >
           -
         </button>
-        <span className="w-6 text-center">{activeQuantity}</span>
+        <span className="w-2 text-center md:w-6">{activeQuantity}</span>
         <button
-          className="flex-1 px-4 py-2 hover:bg-zinc-400/10"
+          className="flex-1 px-2 py-1 hover:bg-zinc-400/10 md:px-4 md:py-2"
           onClick={() => setActiveQuantity(activeQuantity + 1)}
         >
           +
