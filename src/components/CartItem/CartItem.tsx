70c6cd09c97587302d3fcb037952d2a17829a658
Evandro Calado <evandrocalado07@gmail.com>
2024-03-25 22:56:38 -0300

    🐳 chore(components): add context

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import { CartItemProps as CartItemStoreProps } from '@/stores/cartStore';
 import { Trash2 } from 'lucide-react';
 import Image from 'next/image';
 import { FC, useState } from 'react';
 import { Quantity, Separator } from '..';
 
 export interface CartItemProps {
-  className?: string;
+  cartItem: CartItemStoreProps;
 }
 
-export const CartItem: FC<CartItemProps> = () => {
-  const [quantity, setQuantity] = useState(1);
+export const CartItem: FC<CartItemProps> = ({ cartItem }) => {
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
+  const { cover, name, price } = cartItem.product.attributes;
 
   return (
     <>
@@ -18,8 +21,8 @@ export const CartItem: FC<CartItemProps> = () => {
         <div className="flex gap-4">
           <div className="relative h-32 w-32 rounded-lg bg-[#f0f0f0]">
             <Image
-              src="/temp/vector.png"
-              alt="vector"
+              src={cover.data.attributes.formats.thumbnail.url}
+              alt={name}
               fill
               priority
               sizes="(100vw - 2rem) 180px, 280px"
@@ -28,17 +31,19 @@ export const CartItem: FC<CartItemProps> = () => {
           </div>
           <div className="flex flex-col justify-between gap-1">
             <h3 className="text-lg font-semibold capitalize md:text-xl">
-              black t-shirt
+              {name}
             </h3>
             <div className="flex items-center gap-1 text-sm md:text-base">
               <span>Size:</span>
-              <span className="capitalize text-zinc-400">large</span>
+              <span className="capitalize text-zinc-400">{cartItem.size}</span>
             </div>
             <div className="flex items-center gap-1 text-sm md:text-base">
               <span>Color:</span>
-              <span className="capitalize text-zinc-400">white</span>
+              <span className="capitalize text-zinc-400">{cartItem.color}</span>
             </div>
-            <span className="mt-2 text-xl font-bold md:text-2xl">$145</span>
+            <span className="mt-2 text-xl font-bold md:text-2xl">
+              ${price.toFixed(2)}
+            </span>
           </div>
         </div>
 
