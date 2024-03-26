d4c088f87d2e17df72ea1c7699a060bb2a105fc4
Evandro Calado <evandrocalado07@gmail.com>
2024-03-25 22:59:07 -0300

    🐳 chore(stores): add add to cart

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,9 +1,16 @@
 import { ProductType } from '@/types';
 import { create } from 'zustand';
 
+export interface CartItemProps {
+  product: ProductType;
+  color: string;
+  size: string;
+  quantity: number;
+}
+
 export interface CartStoreProps {
-  cartItem: ProductType[];
-  addToCart: (item: ProductType) => void;
+  cartItems: CartItemProps[];
+  addToCart: (product: CartItemProps) => void;
   // removeFromCart: (id: string) => void;
   // increaseQuantity: (id: string) => void;
   // decreaseQuantity: (id: string) => void;
@@ -11,9 +18,9 @@ export interface CartStoreProps {
 }
 
 export const useCartStore = create<CartStoreProps>((set) => ({
-  cartItem: [],
-  addToCart: (item) =>
-    set((state) => ({ cartItem: [...state.cartItem, item] })),
+  cartItems: [],
+  addToCart: (cartItem) =>
+    set((state) => ({ cartItems: [...state.cartItems, cartItem] })),
   // removeFromCart: (id: string) => set((state) => ({ cartItem: state.cartItem.filter((item) => item.id !== id) })),
   // increaseQuantity: (id: string) => set((state) => ({ cartItem: state.cartItem.map((item) => (item.id === id ? { ...item, quantity: item.quantity + 1 } : item)) })),
   // decreaseQuantity: (id: string) => set((state) => ({ cartItem: state.cartItem.map((item) => (item.id === id ? { ...item, quantity: item.quantity - 1 } : item)) })),
