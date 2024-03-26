916d6e4760547eea27df5eeb4c13e2d4c05e5c54
Evandro Calado <evandrocalado07@gmail.com>
2024-03-25 22:55:58 -0300

    🐳 chore(components): add context

--- src/components/CartList/CartList.tsx
+++ src/components/CartList/CartList.tsx
@@ -1,11 +1,17 @@
+import { CartItemProps } from '@/stores/cartStore';
+import { FC } from 'react';
 import { CartItem } from '..';
 
-export const CartList = () => {
+export interface CartListProps {
+  cartItems: CartItemProps[];
+}
+
+export const CartList: FC<CartListProps> = ({ cartItems }) => {
   return (
     <div className="flex h-fit w-full flex-col gap-4 rounded-2xl border-[1px] border-[#f0f0f0] p-4 lg:flex-[2]">
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.product.id} cartItem={cartItem} />
+      ))}
     </div>
   );
 };
