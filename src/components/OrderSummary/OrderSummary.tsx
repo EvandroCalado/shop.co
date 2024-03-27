22c95c2994d4bdc11ac2cd812ba23a4023a09496
Evandro Calado <evandrocalado07@gmail.com>
2024-03-26 21:22:15 -0300

    🐳 chore(components): add totals

--- src/components/OrderSummary/OrderSummary.tsx
+++ src/components/OrderSummary/OrderSummary.tsx
@@ -1,7 +1,36 @@
+'use client';
+
+import { useCartStore } from '@/stores/cartStore';
+import { calcDiscount } from '@/utils/calcDiscount';
 import { ArrowRight, Tag } from 'lucide-react';
 import { Button, Input, Separator } from '..';
 
 export const OrderSummary = () => {
+  const { cartItems } = useCartStore();
+
+  const subtotalWithDiscount = cartItems.reduce((acc, item) => {
+    return (
+      acc +
+      item.quantity *
+        calcDiscount(
+          item.product.attributes.price,
+          item.product.attributes.discount,
+        )
+    );
+  }, 0);
+
+  const subtotalWithoutDiscount = cartItems.reduce((acc, item) => {
+    return acc + item.quantity * item.product.attributes.price;
+  }, 0);
+
+  const totalPercentageDiscount = cartItems.reduce((acc, item) => {
+    return acc + item.product.attributes.discount / cartItems.length;
+  }, 0);
+
+  const totalDiscount = subtotalWithoutDiscount - subtotalWithDiscount;
+
+  const deliveryCost = 15;
+
   return (
     <div className="flex w-full flex-col gap-2 rounded-2xl border-[1px] border-[#f0f0f0] p-4 lg:flex-1">
       <h3 className="text-lg font-semibold capitalize md:text-xl">
@@ -9,22 +38,30 @@ export const OrderSummary = () => {
       </h3>
       <div className="flex items-center justify-between">
         <span className="capitalize text-zinc-400">subtotal</span>
-        <span className="text-lg font-semibold">$145</span>
+        <span className="text-lg font-semibold">
+          ${subtotalWithDiscount.toFixed(2)}
+        </span>
       </div>
       <div className="flex items-center justify-between">
-        <span className="capitalize text-zinc-400">discount(-20%)</span>
-        <span className="text-lg font-semibold text-red-500">-$145</span>
+        <span className="capitalize text-zinc-400">
+          discount(-{totalPercentageDiscount}%)
+        </span>
+        <span className="text-lg font-semibold text-red-500">
+          -${totalDiscount.toFixed(2)}
+        </span>
       </div>
       <div className="flex items-center justify-between">
         <span className="capitalize text-zinc-400">delivery free</span>
-        <span className="text-lg font-semibold">$15</span>
+        <span className="text-lg font-semibold">${deliveryCost}</span>
       </div>
 
       <Separator />
 
       <div className="flex items-center justify-between">
         <span className="capitalize">total</span>
-        <span className="text-2xl font-semibold">$145</span>
+        <span className="text-2xl font-semibold">
+          ${(subtotalWithDiscount + deliveryCost).toFixed(2)}
+        </span>
       </div>
       <div className="flex items-center gap-2">
         <Input
