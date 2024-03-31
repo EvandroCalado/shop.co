47f433c6fb8785eba960d52e53d04b18cb82a67c
Evandro Calado <evandrocalado07@gmail.com>
2024-03-30 23:29:17 -0300

    🐞 fix: fix id to add to cart

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -65,7 +65,7 @@ const ProductDetails = () => {
     }
 
     addToCart({
-      id: product.id + activeColor + activeSize + Date.now(),
+      id: Date.now().toString(),
       product,
       color: activeColor,
       size: activeSize,
