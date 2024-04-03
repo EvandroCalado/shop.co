0396aa9b32bc379372d80af5d753669c353810df
Evandro Calado <evandrocalado07@gmail.com>
2024-04-03 15:42:49 -0300

    🐳 chore: add avatar on type user

---
+++ src/types/userType.ts
@@ -0,0 +1,53 @@
+export interface UserType {
+  id: number;
+  username: string;
+  email: string;
+  provider: string;
+  confirmed: boolean;
+  blocked: boolean;
+  createdAt: string;
+  updatedAt: string;
+  avatar: Avatar;
+}
+
+interface Avatar {
+  id: number;
+  name: string;
+  alternativeText?: string;
+  caption?: string;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl?: string;
+  provider: string;
+  provider_metadata: Providermetadata;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Formats {
+  thumbnail: Thumbnail;
+}
+
+interface Thumbnail {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path?: string;
+  size: number;
+  width: number;
+  height: number;
+  provider_metadata: Providermetadata;
+}
+
+interface Providermetadata {
+  public_id: string;
+  resource_type: string;
+}
