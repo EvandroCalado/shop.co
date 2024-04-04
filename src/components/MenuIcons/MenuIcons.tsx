6c8c21a9f30e10782151074b6ff9ef75cbddeeb0
Evandro Calado <evandrocalado07@gmail.com>
2024-04-03 22:23:53 -0300

    🐳 chore: add user status

--- src/components/MenuIcons/MenuIcons.tsx
+++ src/components/MenuIcons/MenuIcons.tsx
@@ -16,7 +16,7 @@ import { Button } from '..';
 
 export const MenuIcons = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const { data: session } = useSession();
+  const { status } = useSession();
   const cartItems = useCartStore((state) => state.cartItems);
 
   const router = useRouter();
@@ -43,16 +43,22 @@ export const MenuIcons = () => {
         )}
       </button>
 
-      {session ? (
-        <div className="relative">
-          <button
-            className="cursor-pointer rounded-full p-2 duration-150 hover:bg-[#f0f0f0]"
-            data-testid="user"
-            onClick={() => setShowMenu(!showMenu)}
-          >
-            <CircleUser />
-          </button>
+      {status === 'unauthenticated' && (
+        <Link href="/login" className=" bg-">
+          <Button className="px-6 py-2">login</Button>
+        </Link>
+      )}
 
+      <div className="relative">
+        <button
+          className="cursor-pointer rounded-full p-2 duration-150 hover:bg-[#f0f0f0]"
+          data-testid="user"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <CircleUser />
+        </button>
+
+        {status === 'authenticated' && (
           <div
             className={`${showMenu ? 'visible opacity-100' : 'invisible opacity-0'} absolute -bottom-[8rem] right-0 z-10 flex cursor-pointer flex-col gap-2 rounded-lg bg-[#f0f0f0] px-4 py-2 text-zinc-400 shadow-lg`}
           >
@@ -76,12 +82,8 @@ export const MenuIcons = () => {
               logout
             </button>
           </div>
-        </div>
-      ) : (
-        <Link href="/login" className=" bg-">
-          <Button className="px-6 py-2">login</Button>
-        </Link>
-      )}
+        )}
+      </div>
     </div>
   );
 };
