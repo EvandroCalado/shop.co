34f1f645580ab2b3e6b9cfc62f0de2c2c8027d8a
Evandro Calado <evandrocalado07@gmail.com>
2024-04-05 23:07:49 -0300

    🐞 fix: filters conditionals

--- src/hooks/useFilters.ts
+++ src/hooks/useFilters.ts
@@ -1,8 +1,14 @@
 import { getAllColors, getAllProducts, getAllSizes } from '@/actions';
 import { ColorsType, ProductsType, SizesType } from '@/types';
+import { customFetch } from '@/utils/customFetch';
 import { useCallback, useEffect, useState } from 'react';
 
-export const useFilters = ({ activeName }: { activeName?: string }) => {
+export interface UseFilterProps {
+  activeName: string;
+  currentPage: number;
+}
+
+export const useFilters = ({ activeName, currentPage }: UseFilterProps) => {
   const [allColors, setAllColors] = useState<ColorsType>();
   const [allSizes, setAllSizes] = useState<SizesType>();
   const [allProducts, setAllProducts] = useState<ProductsType>();
@@ -12,6 +18,27 @@ export const useFilters = ({ activeName }: { activeName?: string }) => {
   const [activePrice, setActivePrice] = useState(0);
   const [activeColor, setActiveColor] = useState('');
   const [activeSize, setActiveSize] = useState('');
+
+  const populate = '?populate=deep,3';
+  const nameFilter = activeName
+    ? `&filters[name][$containsi]=${activeName}`
+    : '';
+  const clotheFilter = activeClothe
+    ? `&filters[categories][slug][$eq]=${activeClothe}`
+    : '';
+  const priceFilter = activePrice ? `&filters[price][$lte]=${activePrice}` : '';
+  const colorFilter = activeColor
+    ? `&filters[colors][slug][$eq]=${activeColor}`
+    : '';
+  const sizeFilter = activeSize
+    ? `&filters[sizes][slug][$eq]=${activeSize}`
+    : '';
+  const dressStyleFilter = activeDressStyle
+    ? `&filters[dress_style][slug][$eq]=${activeDressStyle}`
+    : '';
+  const pagination = `&pagination[page]=${currentPage}&pagination[pageSize]=6`;
+
+  const url = `${populate}${nameFilter}${clotheFilter}${priceFilter}${colorFilter}${sizeFilter}${dressStyleFilter}${pagination}`;
 
   const getColors = useCallback(async () => {
     const colors = await getAllColors();
@@ -24,31 +51,41 @@ export const useFilters = ({ activeName }: { activeName?: string }) => {
   }, []);
 
   const getProducts = useCallback(async () => {
-    const products = await getAllProducts(activeName);
+    const products = await getAllProducts({
+      loadProducts: {
+        loadAll: async () => await customFetch.get(`/products${url}`),
+      },
+    });
     setAllProducts(products);
-  }, [activeName]);
+  }, [url]);
 
   useEffect(() => {
     getColors();
     getSizes();
     getProducts();
-  }, [getColors, getSizes, getProducts]);
+  }, [getColors, getProducts, getSizes]);
 
   return {
-    allColors,
-    allSizes,
-    allProducts,
-    activeClothe,
-    activeDressStyle,
-    activePrice,
-    activeColor,
-    activeSize,
-    setAllProducts,
-    setActiveClothe,
-    setActiveDressStyle,
-    setActivePrice,
-    setActiveColor,
-    setActiveSize,
+    all: {
+      allColors,
+      allSizes,
+      allProducts,
+    },
+    active: {
+      activeClothe,
+      activeDressStyle,
+      activePrice,
+      activeColor,
+      activeSize,
+    },
+    setActive: {
+      setAllProducts,
+      setActiveClothe,
+      setActiveDressStyle,
+      setActivePrice,
+      setActiveColor,
+      setActiveSize,
+    },
     getProducts,
   };
 };
