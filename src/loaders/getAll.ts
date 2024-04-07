afd132e9441fa60d20edd9f1fa1de8963ea11c4c
Evandro Calado <evandrocalado07@gmail.com>
2024-04-06 23:15:10 -0300

    ✨ feat: create loaders get all

---
+++ src/loaders/getAll.ts
@@ -0,0 +1,38 @@
+import { AxiosError } from 'axios';
+
+export enum HttpStatusCode {
+  OK = 200,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  SERVER_ERROR = 500,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type HttpResponse<T = any> = {
+  statusCode: HttpStatusCode;
+  body: T;
+};
+
+export type LoadItems = {
+  loadAll: () => Promise<HttpResponse>;
+};
+
+export interface GetAllProductsProps {
+  loadAllItems: LoadItems;
+}
+
+export const getAll = async ({ loadAllItems }: GetAllProductsProps) => {
+  try {
+    const response = await loadAllItems.loadAll();
+
+    if (!(response.statusCode === HttpStatusCode.OK)) {
+      throw new Error('No products found');
+    }
+
+    return response.body;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.response?.data?.error?.message);
+    }
+  }
+};
