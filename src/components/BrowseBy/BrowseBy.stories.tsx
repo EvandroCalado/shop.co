f94d088fedf4c92cea852c40b3a019197dde1f96
Evandro Calado <evandrocalado07@gmail.com>
2024-04-08 09:24:48 -0300

    📖 story: add mock

--- src/components/BrowseBy/BrowseBy.stories.tsx
+++ src/components/BrowseBy/BrowseBy.stories.tsx
@@ -1,11 +1,17 @@
-import { BrowseBy } from '.';
+import { StoryFn } from '@storybook/react';
+import { BrowseBy, BrowseByProps } from '.';
+
+import mock from '../../mocks/productsByDressStyle.json';
 
 export default {
   title: 'Components/BrowseBy',
   component: BrowseBy,
+  args: {
+    productsByDressStyle: mock,
+  },
   parameters: {
     layout: 'fullscreen',
   },
 };
 
-export const Default = () => <BrowseBy />;
+export const Default: StoryFn<BrowseByProps> = (args) => <BrowseBy {...args} />;
